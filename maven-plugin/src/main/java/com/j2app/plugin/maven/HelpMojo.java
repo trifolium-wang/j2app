f16dc5f9f19518aaeb295527f407de9dde23a7e8
wangxudong_mac <xudong1113work@qq.com>
2018-11-28 18:58:53 +0800

    第一次提交项目

---
+++ maven-plugin/src/main/java/com/j2app/plugin/maven/HelpMojo.java
@@ -0,0 +1,23 @@
+package com.j2app.plugin.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * @title:
+ * @author: wangxudong
+ * @date: 2018/11/28 13:32
+ * @version: 1.0
+ * @modified :
+ */
+@Mojo(name="help")
+public class HelpMojo extends AbstractMojo{
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        Log log = getLog();
+        log.info("help!help!help!help!help!help!help!");
+    }
+}
